5bc8872c09c128405b7affe5968f3307da56f4a7
Idrisse Angama <idrisseangama2a@gmail.com>
2023-09-26 18:05:10 +0200

    Add a calculator class

---
+++ CountOnMe/Model/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by Idrisse Angama on 26/09/2023.
+//  Copyright © 2023 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    
+}
