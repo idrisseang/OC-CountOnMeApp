16ba23712e3b4adab314e20a7971225930e17eda
Idrisse Angama <idrisseangama2a@gmail.com>
2023-09-26 18:25:21 +0200

    Add a CalculatorTestCase file

---
+++ CountOnMeTests/CalculatorTestCase.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorTestCase.swift
+//  CountOnMeTests
+//
+//  Created by Idrisse Angama on 26/09/2023.
+//  Copyright © 2023 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+
+final class CalculatorTestCase: XCTestCase {
+
+}
